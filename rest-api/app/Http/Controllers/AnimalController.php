9647cf2a75d185be703be326982ed2efa7766de3
Syiful047 <chakdarwis@gmail.com>
2023-10-27 09:55:28 +0700

    pertemuan5

---
+++ rest-api/app/Http/Controllers/AnimalController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    //property animals
+    public $animals = [];
+
+    // method untuk menampilkan semua hewan
+    public function index(){
+        echo"Menampilkan data animas";
+
+        // loop property animals
+        foreach($this->animals as $animal) {
+            echo "- $animal <br>";
+        }
+    }
+
+    // method untuk menambahkan data
+    public function store(request $request) {
+        echo "Menambahkan hewan baru <br>";
+
+        // menambahkan data ke property animals
+        array_push($this->animals, $request->animal);
+
+        // panggil method index
+        $this->index();
+    }
+
+    // method untuk mengedit data hewan
+    public function update($id, $request){
+        echo "Mengupdate data hewan id $id. <br>";
+
+        // update di property animals
+        $this->animals[$id] = $request->animals;
+
+        // panggil methode index
+        $this->index;
+    }
+
+    // method untuk menghapus data hewan
+    public function destroy($id){
+        echo "Menghapus date hewan id $id <br>";
+
+        unset($this->animals[$id]);
+
+        // panggil method index
+        $this->index;
+    }
+}
---
+++ rest-api/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// animal method get
+Route:: get('/animals', [AnimalController::class, 'index']);
+
+// animal method post
+Route:: get('/animals', [AnimalController::class, 'store']);
+
+// animal method put
+Route:: get('/animals/{id}', [AnimalController::class, 'update']);
+
+// animal method delete
+Route:: get('/animals/{id}', [AnimalController::class, 'destroy']);
+
+// student method post all
+Route::post('/students', [StudentController::class, 'store']);
