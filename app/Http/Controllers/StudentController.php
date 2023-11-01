84f5ae0482f283c95606fc9b0181e94b0a785c81
Syiful047 <chakdarwis@gmail.com>
2023-11-01 17:52:22 +0700

    nambah update & delete

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -20,4 +20,86 @@ class StudentController extends Controller
         // kirim data (json) dan  response code
         return response()->json($data, 200);
     }
+
+    public function store(Request $request)
+    {
+        // menangkap data request
+        $input = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan
+        ];
+
+        // menggunakan model Student untuk insert data
+        $student = Student::create($input);
+
+        $data = [
+            'message' => 'Data berhasil ditambahkan',
+            'data' => $student,
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        // menangkap id dari parameter
+        $student = Student::find($id);
+        // cek apakah ada student dengan id tersebut
+        if (!$student) {
+            $data = [
+                'message' => 'Data tidak ditemukan',
+            ];
+            return response()->json($data, 404);
+        }
+
+        $student->update($request->all());
+
+        // // menangkap data request
+        // $nama = $request->input('nama');
+        // $nim = $request->input('nim');
+        // $email = $request->input('email');
+        // $jurusan = $request->input('jurusan');
+
+        // // mengupdate nilai atribut pada student
+        // $student->nama = $nama;
+        // $student->nim = $nim;
+        // $student->email = $email;
+        // $student->jurusan = $jurusan;
+
+
+        //Menyimpan data yang telah diubah
+        $student->save();
+
+        $data = [
+            'message' => 'Data Berhasil Diubah',
+            'data' => $student
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id)
+
+    {
+        // mencari data siswa berdasarkan id
+        $student = Student::find($id);
+
+        // mengecek apakah data tersebut ada atau tidak
+        if (!$student) {
+            $data = [
+                'message' => 'Data tidak berhasil ditemukan',
+            ];
+
+            return response()->json($data, 404);
+        }
+
+        // menghapus data siswa 
+        $student->delete();
+
+        $data = [
+            'message' => 'Data berhasil dihapus',
+        ];
+        return response()->json($data, 203);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -35,3 +35,6 @@ Route:: get('/animals/{id}', [AnimalController::class, 'destroy']);
 // studentcontroller
 // route untuk menapilkan mahasisawa all
 Route::get('students', [StudentController::class, 'index']);
+Route::post('students', [StudentController::class, 'store']);
+Route::put('students/{id}', [StudentController::class, 'update']);
+Route::delete('students/{id}', [StudentController::class, 'destroy']);
