dac19972d37c3ab86ce283f3526f0b004d1a7405
Syiful047 <chakdarwis@gmail.com>
2023-12-29 10:46:05 +0700

    update

--- Pertemuan12/task/models/Student.js
+++ Pertemuan12/task/models/Student.js
@@ -25,14 +25,50 @@ class Student {
    * Method menerima parameter data yang akan diinsert.
    * Method mengembalikan data student yang baru diinsert.
    */
-  static create() {
+  static create(data) {
     // code here
     return new Promise((resolve, reject) => {
-      const sql = "INSERT INTO students (nama, nim, email, jurusan) VALUES (?,?,?,?)";
+      const sql =
+        "INSERT INTO students (nama, nim, email, jurusan) VALUES (?,?,?,?)";
       const value = [data.nama, data.nim, data.email, data.jurusan];
       db.query(sql, value, (err, result) => {
         resolve(result);
-      })
+      });
+    });
+  }
+
+  // mengupdate data student
+  static async update(id, data) {
+    await new Promise((resolve, reject) => {
+      const sql = "UPDATE students SET ? WHERE id = ?";
+      db.query(sql, [data, id], (err, results) => {
+        resolve(results);
+      });
+    });
+    // mencari data yang baru diupdate
+    const student = await this.find(id);
+    return student;
+  }
+  
+  // menghapus data dari database
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      const sql = "DELETE FROM students WHERE id = ?";
+      db.query(sql, id, (err, results) => {
+        resolve(results);
+      });
+    });
+  }
+  
+  // mencari data berdasarkan id
+  static find(id) {
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT * FROM students WHERE id = ?";
+      db.query(sql, id, (err, results) => {
+        // destructing array
+        const [student] = results;
+        resolve(student);
+      });
     });
   }
 }
